8a3d025b69cee43d0a83ce0f453af728d4e23865
Akansha Singh <47236824+akansha-singh@users.noreply.github.com>
2022-04-28 10:17:08 +0530

    Update longest common subsequence.cpp

--- LCS/longest common subsequence.cpp
+++ LCS/longest common subsequence.cpp
@@ -2,7 +2,8 @@
 // y= a b f d g h r
 // longest common subsequence-> a b d g h  , have to return length
 // longest common substring-> a b 
-
+// brute force approach will be O(n * 2n)
+// naive recursive approach is O(2^n) bcoz at every step we have 2 choices
 
 // Recursive
 class Solution{
@@ -24,6 +25,7 @@ public:
 };
 
 // Memoization
+//O(mn) 
 class Solution{
 public:
     int rec(string x,string y,int n,int m,vector<vector<int>>&t){
